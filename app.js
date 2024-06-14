4bb61a7aaaae16f701fa59929cb9f9203ffd747b
venky916 <venkateshsmsv1999@gmail.com>
2024-06-14 18:26:46 +0530

    third-commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1 = React.createElement('h1',{},"Heading 1 for parcel");
-const heading2 = React.createElement('h2', {}, "heading 2");
-const container = React.createElement('div', {
-    id: "container",
-}, [heading1,heading2]);
-// console.log(heading);
+//React element
+const heading = (<h1 id="title" key="h2"> Namaste React</h1>);
+let xyz=10;
+const Title =() => (<h1 id="title" key="h3"> Title React</h1>);
+//React Component
+const HeaderComponent = () => {
+    return (<div>
+        {heading}
+        <Title/> //Component composition
+        {Title()},{console.log("hello")},{xyz}
+        <h1>Namaste React Functional Component</h1>
+        <h2>this is h2</h2>
+    </div>);
+};
+
+const HeaderComponent2 = () =>
+(<div>
+    <h1>Nameste React Functional Component</h1>
+    <h2>this is h2</h2>
+</div>);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
+root.render(<HeaderComponent />);
