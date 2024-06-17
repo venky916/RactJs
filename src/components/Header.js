4cc1df90554fd8f832069b2ae9fdd3ed2268b7b3
venky916 <venkateshsmsv1999@gmail.com>
2024-06-17 12:39:41 +0530

    "Fifth commit"

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+//functional component
+export const Title = () => (
+    <a href='/'> <img className="logo" src='https://th.bing.com/th/id/R.dd4d1acde8fc22ed40fbce3acde6e99a?rik=%2b8JTWnyz2QTWFg&riu=http%3a%2f%2ffoodvilla.no%2fsrc%2fimg%2flogo.png&ehk=Wphc3mBmMYs0rOR4MjkO1zykcVnnjbx6MEDy3h1PSkw%3d&risl=&pid=ImgRaw&r=0' alt='logo' /></a>
+);
+
+
+//React Component
+const Header = () => {
+    return (
+        <div className='header'>
+            <Title />
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+
+export default Header;
